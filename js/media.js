9f4714ac6e0c60c8ae6acf3da5b4d9c210d33496
Edcley Silva <>
2020-09-07 22:09:46 -0300

    no message

--- js/media.js
+++ js/media.js
@@ -1,10 +1,18 @@
 var left=200,right=500,database,interOn,opcoes=[],LayerRange,layerTuto4,dataset,max,featurename,selecionados=[],medias=[],hops=true;
 var mapVis02 = L.map('vis02',{zoomControl: false,preferCanvas: true,attributionControl: false,crs: L.CRS.Simple}).setView([0.203125,0.6640625], 6);
 var mapVisPerguntas = L.map('visPerguntas',{zoomControl: false,preferCanvas: true,attributionControl: false,crs: L.CRS.Simple}).setView([0.203125,0.6640625], 6);
+mapVis02.dragging.disable();
+mapVis02.touchZoom.disable();
 mapVis02.doubleClickZoom.disable();
 mapVis02.scrollWheelZoom.disable();
+mapVis02.boxZoom.disable();
+mapVis02.keyboard.disable();
+mapVisPerguntas.dragging.disable();
+mapVisPerguntas.touchZoom.disable();
 mapVisPerguntas.doubleClickZoom.disable();
 mapVisPerguntas.scrollWheelZoom.disable();
+mapVisPerguntas.boxZoom.disable();
+mapVisPerguntas.keyboard.disable();
 var grades;
 var bounds = [[0,0], [1000,1000]];
 var geodata;
@@ -44,8 +52,8 @@ function Start_Update_data(){
       }
     }
     //
-    colorScale = d3.scale.quantile().domain([menor,maior]).range(['#f7fcfd','#e5f5f9','#ccece6','#99d8c9','#66c2a4','#41ae76','#238b45','#006d2c','#00441b']); 
-    grades = d3.scale.linear().domain([menor,maior]).ticks(12);
+    colorScale = d3.scaleQuantize().domain([menor,maior]).range(['#f7fcfd','#e5f5f9','#ccece6','#99d8c9','#66c2a4','#41ae76','#238b45','#006d2c','#00441b']); 
+    grades = colorScale.ticks();
 
     addLegend();
     distribution_data=Object.keys(dist).map(function(key) {
@@ -174,59 +182,62 @@ function VisPerguntas(){
     if(layerPerguntas!= undefined){
       layerPerguntas.clearLayers();
     }
-    layerPerguntas=L.geoJson(geodata,
-      {style: function(feature){
-      if(amostraN!=undefined){
-        var prob_gerada= distribuicaoSin(feature.properties.id,distribution_data)[amostraN];
-      }else{
-        var probArea= new distribuicaoTeste(distribuicaoSin(feature.properties.id,distribution_data),0);
-        var prob_gerada= probArea.media().toFixed(2);
-      }
-      if(feature.properties.highlight==1){
-          if(feature.properties.id==0){
-            return {
-              weight: 3.5,
-              opacity: 1,
-              fillColor: color(prob_gerada),
-              fillOpacity: 0.9,
-              color: '#e66101'
-            };
-          }else{
-            return {
-              weight: 3.5,
-              opacity: 1,
-              fillColor: color(prob_gerada),
-              fillOpacity: 0.9,
-              color: '#d01c8b'
-            };
-          }
+    var a=distribuicaoSin(geodata.features.length-1,distribution_data);
+    if(a[0]!=undefined){
+      layerPerguntas=L.geoJson(geodata,
+        {style: function(feature){
+        if(amostraN!=undefined){
+          var prob_gerada= distribuicaoSin(feature.properties.id,distribution_data)[amostraN];
         }else{
+          var probArea= new distribuicaoTeste(distribuicaoSin(feature.properties.id,distribution_data),0);
+          var prob_gerada= probArea.media().toFixed(2);
+        }
+        if(feature.properties.highlight==1){
+            if(feature.properties.id==0){
               return {
-                weight: 0.5,
+                weight: 3.5,
                 opacity: 1,
                 fillColor: color(prob_gerada),
-                color: 'black',
-                fillOpacity: 0.9
+                fillOpacity: 0.9,
+                color: '#e66101'
               };
-          }
-    },
-    onEachFeature: function (feature, layer) {
-          layer.on('mouseover', function (e) {
-              highlightFeature(e);
-          });
-          layer.on('mouseout', function (e) {
-              layerPerguntas.resetStyle(e.target);
-          });
-      }
-  }).addTo(mapVisPerguntas);
-  if(amostraN!=undefined){
-    infoVisPerguntas.update = function (props) {
-      this._div.innerHTML= '<h5>Informações gerais.</h5>' +  (props ?'<b>' + props.name + '</b><br />' + props.density + ' people / mi<sup>2</sup>': ' Amostra Nº'+amostraN+'.');
-    };
-  }else{
-    infoVisPerguntas.update = function (props) {
-      this._div.innerHTML= infoprops(props);
-    };
+            }else{
+              return {
+                weight: 3.5,
+                opacity: 1,
+                fillColor: color(prob_gerada),
+                fillOpacity: 0.9,
+                color: '#d01c8b'
+              };
+            }
+          }else{
+                return {
+                  weight: 0.5,
+                  opacity: 1,
+                  fillColor: color(prob_gerada),
+                  color: 'black',
+                  fillOpacity: 0.9
+                };
+            }
+      },
+      onEachFeature: function (feature, layer) {
+            layer.on('mouseover', function (e) {
+                highlightFeature(e);
+            });
+            layer.on('mouseout', function (e) {
+                layerPerguntas.resetStyle(e.target);
+            });
+        }
+    }).addTo(mapVisPerguntas);
+    if(amostraN!=undefined){
+      infoVisPerguntas.update = function (props) {
+        this._div.innerHTML= '<h5>Informações gerais.</h5>' +  (props ?'<b>' + props.name + '</b><br />' + props.density + ' people / mi<sup>2</sup>': ' Amostra Nº'+amostraN+'.');
+      };
+    }else{
+      infoVisPerguntas.update = function (props) {
+        this._div.innerHTML= infoprops(props);
+      };
+    }
+    infoVisPerguntas.addTo(mapVisPerguntas);
   }
-  infoVisPerguntas.addTo(mapVisPerguntas);
 }
